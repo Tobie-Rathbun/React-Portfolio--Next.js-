f58c659315600050600405f710d18b8f1a6cbd78
Tobie-Rathbun <tsrathbun@gmail.com>
2024-12-18 10:13:36 -0700

    added dependencies, modified server

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const serverless = require('serverless-http');
+
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.json({ message: 'Hello from Express deployed on Netlify!' });
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from serverless function!' });
 });
 
+module.exports = app;
 module.exports.handler = serverless(app);

3075986c7c339d684c9b8e194b9c5a121b0d7d60
Tobie-Rathbun <tsrathbun@gmail.com>
2024-12-29 15:11:02 -0700

    update, 404 page made dynamic, netlify deploy 0.04

---
+++ src/app/404.tsx
@@ -0,0 +1,16 @@
+// src/app/404.tsx or src/pages/404.tsx
+import React from 'react';
+
+export const dynamic = 'force-dynamic';
+
+
+const Custom404 = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default Custom404;

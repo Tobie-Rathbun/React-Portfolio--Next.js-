bfd2fc073355777dd97cc28632b8f21d8d805017
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-01 09:33:51 -0700

    styled home page animated cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,20 @@ import Link from "next/link";
 import './globals.css';
 
 export default function Home() {
-  const [scope, animate] = useAnimate();
+  const [scope1, animate1] = useAnimate();
+  const [scope2, animate2] = useAnimate();
 
-  function handleHover() {
+  function handleHover(animate: typeof animate1, scope: React.RefObject<HTMLDivElement>) {
     animate([
       [scope.current, { rotate: -90 }],
       [scope.current, { scale: 1.15 }],
       [scope.current, { rotate: 0 }],
-      [scope.current, { scale: 1.5 }]
+      [scope.current, { scale: 1.5 }],
     ]);
   }
 
-  function handleLeave() {
-    animate([
-      [scope.current, { scale: 1 }]
-    ]);
+  function handleLeave(animate: typeof animate1, scope: React.RefObject<HTMLDivElement>) {
+    animate([[scope.current, { scale: 1 }]]);
   }
 
   return (
@@ -38,11 +37,12 @@ export default function Home() {
         </p>
 
         <div className="cards-container">
+          {/* Card 1 */}
           <div
             className="animated-card"
-            ref={scope}
-            onMouseEnter={handleHover}
-            onMouseLeave={handleLeave}
+            ref={scope1}
+            onMouseEnter={() => handleHover(animate1, scope1)}
+            onMouseLeave={() => handleLeave(animate1, scope1)}
           >
             <Link href="/rps" style={{ textDecoration: 'none' }}>
               <div className="card-content">
@@ -54,7 +54,33 @@ export default function Home() {
               </div>
             </Link>
           </div>
+
+          {/* Card 2 */}
+          <div
+            className="animated-card"
+            ref={scope2}
+            onMouseEnter={() => handleHover(animate2, scope2)}
+            onMouseLeave={() => handleLeave(animate2, scope2)}
+          >
+            <Link href="/rps" style={{ textDecoration: 'none' }}>
+              <div className="card-content">
+              <h2>
+                  <span className="emoji">&#9824;&#65039;</span>
+                  <span className="emoji"id="heart">&#9829;&#65039;</span>
+                  <span className="emoji">&#9827;&#65039;</span>
+                </h2>
+              </div>
+            </Link>
+          </div>
         </div>
+
+        <div className="card-title">
+            <h1>Rock, Paper, Scissors</h1>
+            <h1 className="card-poker">Texas Hold' Em</h1>
+        </div>
+
+
+
       </div>
     </>
   );

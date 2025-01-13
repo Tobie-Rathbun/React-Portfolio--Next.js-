7513c1888a5412ac00d85c4d21d3ab7cad17c0d5
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-13 16:55:31 -0700

    created new page for future invite card project

---
+++ src/app/invite/page.tsx
@@ -0,0 +1,18 @@
+export default function ResumePage() {
+    return (
+      <div className="resume-page">
+        <object
+          data="/Tobie Rathbun Resume January 2025.pdf"
+          type="application/pdf"
+          width="100%"
+          height="800px"
+        >
+          <p>
+            Your browser does not support viewing PDFs.{" "}
+            <a href="/Tobie Rathbun Resume January 2025.pdf">Download the resume</a>.
+          </p>
+        </object>
+      </div>
+    );
+  }
+  
